17a15a5aa85213b9b17e8e6c72aef3bc4dadc916
Donleave <980472712@qq.com>
2024-01-13 20:00:39 +0800

    bean-definition-and-bean-definition-registry

---
+++ src/main/java/org/springframework/beans/factory/config/SingletonBeanRegistry.java
@@ -0,0 +1,12 @@
+package org.springframework.beans.factory.config;
+
+/**
+ * 单例注册表
+ *
+ * @author Donleave
+ * @data 2024/1/13
+ */
+public interface SingletonBeanRegistry {
+
+	Object getSingleton(String beanName);
+}
