c4522d29194df97b8b0bd0000b9c0a243e7bdf51
Donleave <980472712@qq.com>
2024-01-26 15:54:47 +0800

    基于JDK的动态代理

---
+++ src/main/java/org/springframework/aop/framework/ReflectiveMethodInvocation.java
@@ -0,0 +1,50 @@
+package org.springframework.aop.framework;
+
+import org.aopalliance.intercept.MethodInvocation;
+
+import java.lang.reflect.AccessibleObject;
+import java.lang.reflect.Method;
+
+/**
+ *当代理对象是采用jdk动态代理创建的，通过代理对象来访问目标对象的方法的时，
+ * 最终过程是由ReflectiveMethodInvocation来处理的，内部会通过递归调用方法拦截器，最终会调用到目标方法。
+ */
+public class ReflectiveMethodInvocation implements MethodInvocation {
+
+	private final Object target;
+
+	private final Method method;
+
+	private final Object[] arguments;
+
+	public ReflectiveMethodInvocation(Object target, Method method, Object[] arguments) {
+		this.target = target;
+		this.method = method;
+		this.arguments = arguments;
+	}
+
+	@Override
+	public Object proceed() throws Throwable {
+		return method.invoke(target, arguments);
+	}
+
+	@Override
+	public Method getMethod() {
+		return method;
+	}
+
+	@Override
+	public Object[] getArguments() {
+		return arguments;
+	}
+
+	@Override
+	public Object getThis() {
+		return target;
+	}
+
+	@Override
+	public AccessibleObject getStaticPart() {
+		return method;
+	}
+}
