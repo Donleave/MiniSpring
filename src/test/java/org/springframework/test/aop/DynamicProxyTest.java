c5670039b66b4208fef30c7012b2976d7795edc6
Donleave <980472712@qq.com>
2024-01-27 14:02:24 +0800

    常用的Advice

--- src/test/java/org/springframework/test/aop/DynamicProxyTest.java
+++ src/test/java/org/springframework/test/aop/DynamicProxyTest.java
@@ -5,10 +5,12 @@ import org.junit.Test;
 import org.springframework.aop.AdvisedSupport;
 import org.springframework.aop.MethodMatcher;
 import org.springframework.aop.TargetSource;
+import org.springframework.aop.adapter.MethodBeforeAdviceInterceptor;
 import org.springframework.aop.aspectj.AspectJExpressionPointcut;
 import org.springframework.aop.framework.CglibAopProxy;
 import org.springframework.aop.framework.JdkDynamicAopProxy;
 import org.springframework.aop.framework.ProxyFactory;
+import org.springframework.test.common.WorldServiceBeforeAdvice;
 import org.springframework.test.common.WorldServiceInterceptor;
 import org.springframework.test.service.WorldService;
 import org.springframework.test.service.WorldServiceImpl;
@@ -52,6 +54,17 @@ public class DynamicProxyTest {
 		// 使用CGLIB动态代理
 		advisedSupport.setProxyTargetClass(true);
 		proxy = (WorldService) new ProxyFactory(advisedSupport).getProxy();
+		proxy.explode();
+	}
+
+	@Test
+	public void testBeforeAdvice() throws Exception {
+		//设置BeforeAdvice
+		WorldServiceBeforeAdvice beforeAdvice = new WorldServiceBeforeAdvice();
+		MethodBeforeAdviceInterceptor methodInterceptor = new MethodBeforeAdviceInterceptor(beforeAdvice);
+		advisedSupport.setMethodInterceptor(methodInterceptor);
+
+		WorldService proxy = (WorldService) new ProxyFactory(advisedSupport).getProxy();
 		proxy.explode();
 	}
 }
